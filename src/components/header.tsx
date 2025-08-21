b63fcbf3e1a839b454f67b7682e6df56ea8d2e32
AgustinAnichini <agustinanichini277@gmail.com>
2025-08-21 18:37:06 -0300

    Menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,19 @@ export default function Header() {
     
     return (
         <>
-            <nav>
-                <div>
-
+            <nav className="flex justify-between items-baseline bg-white-800 text-white p-5 shadow-md">
+                <div className="flex items-center pl-20">
+                    <h1 className="text-black text-2xl">Pixelar</h1>
                 </div>
                 
-                <div>
-
+                <div className="flex justify-around pr-20">
+                    <ul className="flex space-x-7 text-black">
+                        <li><a href="/" className="hover:text-blue-500">Inicio</a></li>
+                        <li><a href="/servicios" className="hover:text-blue-500 ">Servicios</a></li>
+                        <li><a href="/proyectos" className="hover:text-blue-500">Proyectos</a></li>
+                        <li><a href="/nosotros" className="hover:text-blue-500">Nosotros</a></li>
+                        <li><a href="/contacto" className="hover:text-blue-500">Contacto</a></li>
+                    </ul>
                 </div>
             </nav>
         </>
