5ebcba03501da630efd64c9d375c609ac9124226
Martin de Oro <deoromartinignacio@gmail.com>
2025-08-26 18:27:27 -0300

    se agregan las distintas secciones en conjunto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import ContactoSection from './components/ContactoSection'
+import Footer from './components/Footer'
+import NuestraMetodologia from './components/NuestraMetodologia'
 import PortfolioGrid from './components/PortfolioGrid'
+import SobreNosotros from './components/SobreNosotros'
 
 function App() {
 
   return (
     <>
       <PortfolioGrid />
+      <SobreNosotros />
+      <NuestraMetodologia />
+      <ContactoSection />
+      <Footer />
     </>
   )
 }
