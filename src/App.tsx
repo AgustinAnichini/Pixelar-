6017eccf5b38c3f75acf94720bb8cd9e9a767395
AgustinAnichini <agustinanichini277@gmail.com>
2025-09-01 14:16:48 -0300

    boton de whatsapp e ir arriba

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,32 @@ import Footer from './components/Footer'
 import NuestraMetodologia from './components/NuestraMetodologia'
 import PortfolioGrid from './components/PortfolioGrid'
 import SobreNosotros from './components/SobreNosotros'
+import { useEffect, useState } from 'react'
 
 
 function App() {
+ const [showScrollToTop, setShowScrollToTop] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowScrollToTop(true);
+      } else {
+        setShowScrollToTop(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+  
   return (
     <>
       <Header/>
@@ -23,6 +45,23 @@ function App() {
       <NuestraMetodologia />
       <ContactoSection />
       <Footer />
+
+      <a
+        href="https://wa.me/5491138721497"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="whatsapp-button"
+        aria-label="Contactar por WhatsApp"
+      >
+        <img src="./whatsapp-icon.svg" alt="boton de whatsapp" className='p-1'/>
+      </a>
+
+      {/* Botón de ir hacia arriba */}
+      {showScrollToTop && (
+        <button onClick={scrollToTop} className="scroll-to-top-button">
+          &#8593;
+        </button>
+      )}
     </>
   )
 }
